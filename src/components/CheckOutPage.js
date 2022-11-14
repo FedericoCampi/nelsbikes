3536b825f28c8d406b1bb97209153745683cfde4
Federico Campi <federicocampi95@gmail.com>
2022-11-14 05:04:32 -0300

    cardEmpty

--- src/components/CheckOutPage.js
+++ src/components/CheckOutPage.js
@@ -1,24 +1,44 @@
-import { Grid, Typography } from '@mui/material'
+import { Button, Grid, Typography } from '@mui/material'
 import React from 'react'
 import CheckOutCard from './CheckOutCard'
 import Total from './Total'
 import { useStateValue } from '../StateProvider';
 import '../styles/CheckOutPage.scss';
+import cEmpty from '../assets/cardEmpty.png'
+import { Link } from 'react-router-dom';
 
 const CheckOutPage = () => {
 
     // eslint-disable-next-line no-unused-vars
     const [{basket}, dispatch] = useStateValue();
 
+    function CardEmpty(){
+        return(
+        <React.Fragment >
+            <div className="cardEmptyContainer">
+                <div className='cardEmptyImg'>
+                    <img src={cEmpty} alt="Card empty">
+                    </img>
+                </div>
+                <div className="cardEmptyDiv">
+                    <h2>Your cart is empty</h2>
+                    <Button component={Link} to='/' variant="outlined">Shop today's deals</Button>
+                </div> 
+            </div>
+        </React.Fragment>
+        )
+    }
     function FormRow(){
         return (
             <React.Fragment>
                 <Grid container spacing={1}>
-                    {
-                        basket?.map((item) => (
-                        <Grid item xs={12} sm={8} md={6} lg={4}>
-                            <CheckOutCard className='FormRow' key={item.id} product={item}/>
-                        </Grid>
+                    {   
+                        basket.length === 0 
+                        ?   <CardEmpty/>
+                        :   basket.map((item) => (
+                            <Grid item xs={12} sm={8} md={6} lg={4}>
+                                <CheckOutCard className='FormRow' key={item.id} product={item}/>
+                            </Grid>
                         ))
                     }
                 </Grid>
